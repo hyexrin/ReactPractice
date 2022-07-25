9b0a476a56e9ed4e56b14c5c29dac46c2174ae9c
HYEXRIN\ohr10 <ohr1003@gmail.com>
2022-07-25 13:24:09 +0900

    react project

---
+++ rockpaper/src/App.js
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import './App.css';
+import Box from "./component/Box"
+
+
+// 1. 박스 2개 (타이틀, 사진, 결과)
+// 2. 가위 바위 보 버튼 3개
+// 3. 버튼을 클릭하면 클릭한 값이 박스에 보임
+// 4. 컴퓨터는 랜덤하게 아이템 선택이 됨
+// 5. 3, 4의 결과를 가지고 누가 이겼는지 승패를 따짐
+// 6. 승패 결과에 따라 테두리 색 변경 (이김: 초록, 짐: 빨강, 비김: 검정)
+
+const choice = {
+  rock:{
+    name:"Rock",
+    img:"https://cdn-icons.flaticon.com/png/512/2338/premium/2338381.png?token=exp=1658721510~hmac=611bcb68ef833f8f93ac72940811680b"
+  },
+  scissors:{
+    name:"Scisoors",
+    img:"https://cdn-icons-png.flaticon.com/128/2107/2107852.png"
+  },
+  paper:{
+    name:"Paper",
+    img:"https://cdn-icons-png.flaticon.com/512/7467/7467471.png"
+  }
+}
+
+function App() {
+
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setcomputerSelect] = useState(null);
+
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setcomputerSelect(computerChoice);
+  };
+
+  const randomChoice = () =>{
+    let itemArray = Object.keys(choice); // 객체에 키 값만 뽑아서 Arrya 만들어주는 함수
+    console.log("item array", itemArray);
+
+    let randomItem = Math.floor(Math.random()*itemArray.length);
+    console.log(randomItem);
+
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
+ 
+  return (
+    <div>
+      <div className='main'>
+      <Box title="You" item={userSelect}/>
+      <Box title="Computer" item={computerSelect}/>
+      </div>
+      <div className='main'>
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
