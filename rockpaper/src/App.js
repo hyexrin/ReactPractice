2adb3daa14300e240e61a4837a93df789da2b483
HYEXRIN\ohr10 <ohr1003@gmail.com>
2022-07-27 17:43:53 +0900

    rockpaper 불필요한 주석 제거

--- rockpaper/src/App.js
+++ rockpaper/src/App.js
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import './App.css';
 import Box from "./component/Box"
 
-// ********** 6 11분 부터
-
 // 1. 박스 2개 (타이틀, 사진, 결과)
 // 2. 가위 바위 보 버튼 3개
 // 3. 버튼을 클릭하면 클릭한 값이 박스에 보임
